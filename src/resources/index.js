141c2924eaa481d2c24c61301939b5b99d8ee492
j.taneichi <j.tane3ma@gmail.com>
2015-12-19 06:46:34 +0900

    update

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,4 +1,5 @@
 export function configure(aurelia) {
     aurelia.globalResources('dropdown.html');    // ViewModel 省略したなら .html をつけよう
+    aurelia.globalResources('nav-bar.html');    // ViewModel 省略したなら .html をつけよう
     // 部品が増えたら同じようにここに追加していく
 }
